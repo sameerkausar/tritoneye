f5c7b7afefb987af0e8418ab25a70099e8106a52
sameerkausar <sameerkausar@gmail.com>
2016-05-20 13:26:02 -0700

    Implemented proper design
    created a method to take car of the car count. It works assuming the input is a two digit number, the first digit being the "hill" and the second being "direction." Hill 0 is between 0 and 1 aka the entrance, hill 1 is between floor 1 and 2, hill 2 between 2 and 3, etc.  the direction input is either 1 or 0. 0 being in and 1 being out.

--- parkcount.py
+++ parkcount.py
@@ -1,22 +1,41 @@
-## if hill = 0, car entered lot
-testCase = ["01","01","11","21","01","20","10"]
-count = 0
-##10 open spots per floor, 5 floors, index 0 is total cars
-spots = [0,10,10,10,10,10]
-
-while count < len(testCase):
-	hill = str(testCase[count])[0]
-	hill = int(hill)
-	direction = str(testCase[count])[1]
-	#print direction
-	direction = int(direction)
+def parkCount(hill,direction,totalCars,potentialSpots):
 	if(direction == 1 ): ## car entered
 		#means hill=1 car goes 1->2
-		spots[hill+1] = spots[hill+1] - 1
-		spots[hill] = spots[hill] + 1
+		totalCars[hill] = totalCars[hill] - 1
+		totalCars[hill+1] = totalCars[hill+1] + 1
 	else:
 		#means hill=1 car goes 2->1
-		spots[hill] = spots[hill] - 1
-		spots[hill+1] = spots[hill+1] + 1
-	count = count+1
-	print spots
+		totalCars[hill] = totalCars[hill] + 1
+		totalCars[hill+1] = totalCars[hill+1] - 1
+	print totalCars
+	openSpots = []
+	first = True #do something different for index 0 aka total car count
+ 	for s, c in zip(potentialSpots, totalCars):
+ 		if first:
+ 			c = -c
+ 			first = False
+		if s+c < 0:
+			openSpots.append(0)
+		else:
+			openSpots.append(s-c)
+	
+	print openSpots
+
+def main():
+	## if hill = 0, car entered lot
+	testCase = ["01","01","11","21","01","20","10","01","01","11"]
+	count = 0
+	##10 open spots per floor, 5 floors, index 0 is total cars
+	potentialSpots = [50,10,10,10,10,10]
+	totalCars = [0,0,0,0,0,0]
+
+	while count < len(testCase):
+		hill = str(testCase[count])[0]
+		hill = int(hill)
+		direction = str(testCase[count])[1]
+		#print direction
+		direction = int(direction)
+		parkCount(hill,direction,totalCars,potentialSpots)
+		count += 1
+
+main()
